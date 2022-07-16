c5e1a0782830ca3723787a0d5cf7a1f1ab26977a
sathish kumar <67854806+sathishds@users.noreply.github.com>
2022-07-16 11:30:50 +0530

    Config base url

--- app_login/myapp/config/config.php
+++ app_login/myapp/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://www";
 $path_parts = pathinfo($_SERVER['SCRIPT_NAME']);
-$config['base_url'] = $protocol.$_SERVER['HTTP_HOST'].'/cftci/app_login/';
+$config['base_url'] = $protocol.$_SERVER['HTTP_HOST'].'/app_login/';
 // $config['base_url'] = 'http://localhost/cftci/app_login/';
 
 
